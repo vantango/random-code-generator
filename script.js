5bb58e58891fbb642d23f878e5363a7676821938
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-09 09:49:47 -0800

    added variable arrays to js file

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//setting values (will not allow " or | in string)
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]"]
+
+//TODO:
+//TODO: When user clicks button, they are presented with a series of prompts for password criteria
+
+//TODO: set password length/complexity
+
